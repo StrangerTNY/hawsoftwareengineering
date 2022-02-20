8cc323643946d0a96e59f3f2f55bf1d30074fc16
Fabian Braun <fabian.braun@haw-hamburg.de>
2022-02-20 21:26:07 +0100

    Tests für PlanerFabrik done

--- FutterplanerB/src/src/PlanerFabrikTest.java
+++ FutterplanerB/src/src/PlanerFabrikTest.java
@@ -1,46 +1,57 @@
 package src;
 
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import Futter.BARF;
 import Futter.Futter;
-import Tiere.Hund;
+import GUI.LandingPage;
+import Tiere.Tier;
 
-
+// Test für die Planerfabrik
 public class PlanerFabrikTest {
     
     PlanerFabrik testPlanerFabrik;
-    Futter testFutter;
+    String hundsString;
 
+    // Instanziierung
     @Before
-    private void setup() {
+    public void setup() {
         testPlanerFabrik = new PlanerFabrik(); 
-        // testFutter = new Futter();
+        hundsString = "Hund";
     }
 
+    // Testet die Methode getFutter auf Funktion
     @Test
     public void testGetFutterSuccess() {
-        assertTrue(testPlanerFabrik.getFutter("Hund") instanceof Futter);
-        assertNotNull(testPlanerFabrik.getFutter("Hund"));
+        assertTrue(testPlanerFabrik.getFutter(hundsString) instanceof ArrayList);
+        assertNotNull(testPlanerFabrik.getFutter(hundsString));
     }
 
+    // Testet die Methode getFutter auf Fehlfunktion
     @Test
     public void testGetFutterFail() {
-        // assertFalse(testPlanerFabrik.getFutter("Hund") instanceof PlanerFabrik);
+        assertFalse(testPlanerFabrik.getFutter(hundsString) instanceof Futter);
     }
 
+    // Testet die Methode getTier auf Funktion
     @Test
     public void testGetTierSucess() {
-
+        assertTrue(testPlanerFabrik.getTier(hundsString) instanceof Tier);
+        assertNotNull(testPlanerFabrik.getTier(hundsString)); 
+        assertNull(testPlanerFabrik.getTier(null));
     }
 
+    // Testet die Methode getTier auf Fehlfunktion
     @Test
     public void testGetTierFail() {
-
+        assertFalse(testPlanerFabrik.getTier(hundsString) instanceof ArrayList);
     }
 }
